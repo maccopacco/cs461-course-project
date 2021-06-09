9a9052464cc643fb73fea298fa49c640d3fd3fa2
Max Dreher <maxdmax101@gmail.com>
2021-06-09 19:03:08 -0400

    Add Registrar - Create user

--- src/app/components/DataPage.js
+++ src/app/components/DataPage.js
@@ -1,12 +1,13 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import MaterialTable from "material-table";
 import {listSchoolUsers} from "../../graphql/queries";
 import {graphqlOperation} from "@aws-amplify/api-graphql";
 import {API} from "@aws-amplify/api";
 import {toast} from "react-toastify";
 import {ActionIcon} from "./ActionIcon";
-import {updateSchoolUser} from "../../graphql/mutations";
+import {createSchoolUser, updateSchoolUser} from "../../graphql/mutations";
 import {getRandomInt} from "../Utilities";
+import Popup from "reactjs-popup";
 
 
 //Enums for different views (like showing classes, previous course reports, blah blah blah)
@@ -92,7 +93,7 @@ export default class DataPage extends Component {
     }
 
     createNewPassword(rowData, context) {
-        const randomPassword = getRandomInt(100_000_000).toString()
+        const randomPassword = this.createPassword()
         API.graphql(graphqlOperation(updateSchoolUser, {
             input: {
                 id: rowData.id,
@@ -107,6 +108,10 @@ export default class DataPage extends Component {
             toast.error(m)
             console.error(m, error)
         })
+    }
+
+    createPassword() {
+        return getRandomInt(100_000_000).toString();
     }
 
     /**
@@ -140,10 +145,78 @@ export default class DataPage extends Component {
             case "Registrar": {
                 buttons = [{
                     title: "Show users", onClick: () => this.registrarShowUsers(here)
-                }]
+                }, {title: "Create user", popup: this.registarCreateUserPopup()}]
             }
         }
         return buttons.map(this.makeButton)
+    }
+
+    registarCreateUserPopup() {
+        const ref = React.createRef()
+        return <Popup ref={ref} className='vertical'
+                      key='createUserPopup' trigger={
+            <button>Create user</button>
+        } position="right center">
+            {this.getPopup(ref)}
+        </Popup>
+    }
+
+    getPopup(popupRef) {
+        const here = this
+        const email = React.createRef()
+        const fname = React.createRef()
+        const lname = React.createRef()
+        const user_type = React.createRef()
+        return <>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input ref={email} id="email"/>
+
+                <label htmlFor="fname">First name: </label>
+                <input ref={fname} id="fname"/>
+
+
+                <label htmlFor="lname">Last name: </label>
+                <input ref={lname} id="lname"/>
+
+                <label htmlFor="user_type">User type: </label>
+                <select ref={user_type} id="user_type">
+                    <option value="REGISTRAR">Registrar</option>
+                    <option value="STUDENT">Student</option>
+                    <option value="INSTRUCTOR">Instructor</option>
+                </select>
+            </div>
+            <button onClick={function () {
+                popupRef.current.close()
+                here.createUser(here, email.current.value,
+                    fname.current.value,
+                    lname.current.value,
+                    user_type.current.value)
+            }}
+            >Submit
+            </button>
+        </>
+    }
+
+    createUser(context, email, first, last, user_type) {
+        API.graphql(graphqlOperation(createSchoolUser, {
+            input: {
+                email: email,
+                first_name: first,
+                last_name: last,
+                user_type: user_type,
+                passwrd: this.createPassword()
+            }
+        })).then(function(data) {
+            const user = data.data.createSchoolUser
+            console.log('New user:', user)
+            context.registrarShowUsers(context)
+            alert(`Password is ${user.passwrd}`)
+        }).catch(function(error) {
+            const m = "Could not generate user"
+            console.error(m,error)
+            toast.error(m)
+        })
     }
 
     registrarShowUsers(context) {
@@ -169,6 +242,9 @@ export default class DataPage extends Component {
     }
 
     makeButton(item, index) {
+        if ('popup' in item) {
+            return item.popup
+        }
         return <button key={index} onClick={() => item.onClick()}>{item.title}</button>
     }
 
